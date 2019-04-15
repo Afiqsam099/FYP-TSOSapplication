15e64dc5d9f37df0625f536a085cf75caf3f7701
Afiqsam099 <101208957@students.swinburne.edu.my>
2019-04-15 12:37:19 +0800

    First commit

---
+++ app/src/main/java/com/example/fyp_targetstationaryapps/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.fyp_targetstationaryapps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
